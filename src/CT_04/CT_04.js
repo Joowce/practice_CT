7c09d9463b75a34c17a51930d831acbd78435577
joy <joy@moduparking.com>
2018-06-17 01:57:03 +0900

    only-question 브랜치 추가

--- src/CT_04/CT_04.js
+++ src/CT_04/CT_04.js
@@ -1,71 +1,10 @@
-
 exports.solveEquation = (a, b, c) => {
-    let D = (Math.pow(b,2)) - 4 * a * c;
-    if (D > 0) {
-        D = Math.sqrt(D);
-        const root1 = (-b + D) / 2 * a;
-        const root2 = (-b - D) / 2 * a;
-        return [root1, root2];
-    } else if (D === 0) {
-        const result = -b / 2 * a;
-        return "중근 =" + result;
-    } else {
-        return "근이 없음 = " + 'NULL';
-    }
-    // 9p. 이차방정식 근 제대로 구하기
-    // 함수 solveEquation
-    // (a,b,c) => 두근이 존재하면 [작은근, 큰근]
-    // 근이 하나이면 [중근]
-    // 없으면 null
-
-
-
 };
 
 exports.getGrade = (grade) => {
-    // 11p. 조건문 연습
-    // 시험점수 입력 받아 학점 부여
-    // 기준은 동일
-    // 함수 getGrade
-    // (grade) => 학점
-    // 출력 : (string) ex) ‘A’, ’B’, …
-    if (grade > 90) {
-        return 'A'
-    } else if (grade > 80 && grade < 89) {
-        return 'B'
-    } else if (grade > 70 && grade < 79) {
-        return 'C'
-    } else if (grade > 60 && grade < 69) {
-        return 'D'
-    } else if (grade < 59) {
-        return 'F'
-    }
 };
 
 exports.getGradeWithTest = (grade, testResult) => {
-    // 함수 getGradeWithTest
-    // (grade, testResult) => 학점
-    // grade : String
-    // testResult : true/false
-    if (grade > 90 && testResult === true ) {
-        return 'A'
-    } else if (grade > 90 && testResult === false) {
-        return 'B'
-    } else if (grade > 80 && grade < 89 && testResult === true) {
-        return 'B'
-    } else if (grade > 80 && grade < 89 && testResult === false) {
-        return 'C'
-    } else if (grade > 70 && grade < 79 && testResult === true) {
-        return 'C'
-    } else if (grade > 70 && grade < 79 && testResult === false) {
-        return 'D'
-    } else if (grade > 60 && grade < 69 && testResult === true) {
-        return 'D'
-    } else if (grade > 60 && grade < 69 && testResult === false) {
-        return 'F'
-    }else if (grade < 59) {
-        return 'F'
-    }
 };
 
 exports.getGradeWithAverage = (grade1, grade2, testResult) => {
--- src/CT_05/CT_05.js
+++ src/CT_05/CT_05.js
@@ -1,40 +1,7 @@
 exports.evenSummation = (N1, N2) => {
-    // 9p. 짝수 합
-    // 함수 evenSummation
-    // (N1, N2) => N1 <= … <= N2 사이에 있는 짝수들의 합
-    // 힌트에서 range라는 함수는 무시하세요 (python 내장함수라 javascript에는 없습니다)
-    const arr = [N1];
-    let value = '';
-    while (N1 <= N2) {
-        N1 =+ 1;
-        arr.push(N1);
-    }
-    for (let i = 0; i < arr.length; i =+1) {
-        if (arr[i]%2 === 0) {
-            value =+ arr[i];
-        }
-    }
-    return value;
 };
 
 exports.drawStar = (n) => {
-    // 10p. 별로 패턴 그리기
-    // 함수 drawStar
-    // (n) => 별
-    // 10페이지 참고할것
-    // print => console.log 사용하시면 됩니다.
-    for (let i = 1; i <= n; i++) {
-        for (let j = 0; j < i; j++) {
-            console.log('*');
-        }
-        console.log('/n');
-    }
-    //반대로 찍기
-    // for (let i = 1; i <= n; i++) {
-    //     for (let j = n; j >= i; j--) {
-    //         console.log('*')
-    //     } console.log("/n")
-    // }
 };
 
 exports.drawTriangle = (n) => {
